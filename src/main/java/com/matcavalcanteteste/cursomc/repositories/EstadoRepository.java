99e76796ba0781db05042c7e00a92ef49713e965
MatheusCavalcante <matheus.cavalcante@ibm.com>
2023-02-20 22:13:43 -0300

    estado e cidade

---
+++ src/main/java/com/matcavalcanteteste/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.matcavalcanteteste.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matcavalcanteteste.cursomc.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+	
+	
+
+}
