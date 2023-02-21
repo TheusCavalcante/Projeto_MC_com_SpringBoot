c41d1f17bf679f4d3d4e704d5660e5e29b002245
MatheusCavalcante <matheus.cavalcante@ibm.com>
2023-02-20 23:48:36 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/matcavalcanteteste/cursomc/resources/ClienteResource.java
+++ src/main/java/com/matcavalcanteteste/cursomc/resources/ClienteResource.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.matcavalcanteteste.cursomc.domain.Categoria;
-import com.matcavalcanteteste.cursomc.services.CategoriaService;
+import com.matcavalcanteteste.cursomc.domain.Cliente;
+import com.matcavalcanteteste.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/categorias")
-public class CatergoriaResource {
+@RequestMapping(value="/clientes")
+public class ClienteResource {
 	
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@GetMapping(value="/{id}")
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
