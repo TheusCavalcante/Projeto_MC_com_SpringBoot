a5c3acabac3348444b4b77f3bb1d481d947ae740
MatheusCavalcante <matheus.cavalcante@ibm.com>
2023-02-20 23:33:18 -0300

    Cliente, TipoCliente, Telefones e Enderecos

---
+++ src/main/java/com/matcavalcanteteste/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.matcavalcanteteste.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matcavalcanteteste.cursomc.domain.Endereco;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+	
+	
+
+}
